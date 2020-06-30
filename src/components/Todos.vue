96951530b35bf684fe1aa205e219fa714376f123
Enkronan <robin@tempura.se>
2020-06-30 21:38:13 +0200

    dev
    added a function for deleting a todo via API and added onclick evnt

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -4,6 +4,7 @@
       <div class="todos">
           <div v-for="todo in allTodos" :key="todo.id" class="todo">
               {{todo.title}}
+              <p @click="deleteTodo(todo.id)">Del</p>
           </div>
       </div>
   </div>
@@ -15,7 +16,7 @@ import { mapGetters, mapActions } from 'vuex';
 export default {
     name: "Todos",
     methods: {
-        ...mapActions(["fetchTodos"])
+        ...mapActions(["fetchTodos", "deleteTodo"])
     },
     computed: mapGetters(['allTodos']),
     created() {
